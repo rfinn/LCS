a0459029f812d9423ec91f79612d5a6665e91efa
rfinn <rfinn>
2020-04-08 18:47:52 -0400

    paper2

--- python/Python3/lcs_paper2.py
+++ python/Python3/lcs_paper2.py
@@ -68,6 +68,82 @@ truncated=np.array([113107,140175,79360,79394,79551,79545,82185,166185,166687,16
 ###########################
 ##### Functions
 ###########################
+colorblind1='#F5793A' # orange
+colorblind2 = '#85C0F9' # light blue
+colorblind3='#0F2080' # dark blue
+def colormass(x1,y1,x2,y2,name1,name2, figname, hexbinflag=False,contourflag=False, \
+             xmin=7.9, xmax=11.6, ymin=-1.2, ymax=1.2, contour_bins = 40, ncontour_levels=5,\
+              xlabel='$\log_{10}(M_\star/M_\odot) $', ylabel='$(g-i)_{corrected} $', color1=colorblind3,color2=colorblind2,\
+             nhistbin=50, alphagray=.1):
+    fig = plt.figure(figsize=(8,8))
+    nrow = 4
+    ncol = 4
+    
+    # for purposes of this plot, only keep data within the 
+    # window specified by [xmin:xmax, ymin:ymax]
+    
+    keepflag1 = (x1 >= xmin) & (x1 <= xmax) & (y1 >= ymin) & (y1 <= ymax)
+    keepflag2 = (x2 >= xmin) & (x2 <= xmax) & (y2 >= ymin) & (y2 <= ymax)
+    
+    x1 = x1[keepflag1]
+    y1 = y1[keepflag1]
+    
+    x2 = x2[keepflag2]
+    y2 = y2[keepflag2]
+    
+    ax1 = plt.subplot2grid((nrow,ncol),(1,0),rowspan=nrow-1,colspan=ncol-1, fig=fig)
+    if hexbinflag:
+        #t1 = plt.hist2d(x1,y1,bins=100,cmap='gray_r')
+        #H, xbins,ybins = np.histogram2d(x1,y1,bins=20)
+        #extent = [xbins[0], xbins[-1], ybins[0], ybins[-1]]
+        #plt.contour(np.log10(H.T+1),  10, extent = extent, zorder=1,colors='k')
+        #plt.hexbin(xvar2,yvar2,bins='log',cmap='Blues', gridsize=100)
+
+        plt.hexbin(x1,y1,bins='log',cmap='gray_r', gridsize=75,label=name1)
+    else:
+        plt.plot(x1,y1,'k.',color=color1,alpha=alphagray,label=name1, zorder=2)
+    if contourflag:
+        H, xbins,ybins = np.histogram2d(x2,y2,bins=contour_bins)
+        extent = [xbins[0], xbins[-1], ybins[0], ybins[-1]]
+        plt.contour((H.T), levels=ncontour_levels, extent = extent, zorder=1,colors=color2, label='__nolegend__')
+        #plt.legend()
+    else:
+        plt.plot(x2,y2,'c.',color=color2,alpha=.3, label=name2)
+        
+        
+        #plt.legend()
+    #sns.kdeplot(agc['LogMstarTaylor'][keepagc],agc['gmi_corrected'][keepagc])#,bins='log',gridsize=200,cmap='blue_r')
+    #plt.colorbar()
+    
+    plt.axis([xmin,xmax,ymin,ymax])
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.xlabel(xlabel,fontsize=22)
+    plt.ylabel(ylabel,fontsize=22)
+    #plt.axis([7.9,11.6,-.05,2])
+    ax2 = plt.subplot2grid((nrow,ncol),(0,0),rowspan=1,colspan=ncol-1, fig=fig, sharex = ax1, yticks=[])
+    t = plt.hist(x1, normed=True, bins=nhistbin,color=color1,histtype='step',lw=1.5, label=name1)
+    t = plt.hist(x2, normed=True, bins=nhistbin,color=color2,histtype='step',lw=1.5, label=name2)
+    #plt.legend()
+    ax2.legend(fontsize=10,loc='upper left')
+    ax2.xaxis.tick_top()
+    ax3 = plt.subplot2grid((nrow,ncol),(1,ncol-1),rowspan=nrow-1,colspan=1, fig=fig, sharey = ax1, xticks=[])
+    t=plt.hist(y1, normed=True, orientation='horizontal',bins=nhistbin,color=color1,histtype='step',lw=1.5, label=name1)
+    t=plt.hist(y2, normed=True, orientation='horizontal',bins=nhistbin,color=color2,histtype='step',lw=1.5, label=name2)
+    
+    plt.yticks(rotation='horizontal')
+    ax3.yaxis.tick_right()
+    plt.savefig(figname)
+
+    print('############################################################# ')
+    print('KS test comparising galaxies within range shown on the plot')
+    print('')
+    print('STELLAR MASS')
+    t = ks(x1,x2,run_anderson=False)
+    print('')
+    print('COLOR')
+    t = ks(y1,y2,run_anderson=False)
+
 
 def plotsalim07():
     #plot the main sequence from Salim+07 for a Chabrier IMF
@@ -187,7 +263,7 @@ class gswlc_base():
         plt.xlabel('logMstar',fontsize=16)
         plt.ylabel('logSFR',fontsize=16)
         
-    def plot_positions(self, plotsingle = True):
+    def plot_positions(self,plotsingle=True, filename=None):
         if plotsingle:
             plt.figure(figsize=(14,8))
 
@@ -200,7 +276,9 @@ class gswlc_base():
         plt.axhline(y=60)
         plt.axvline(x=135)
         plt.axvline(x=225)
-
+        #plt.axis([130,230,0,65])
+        if filename is not None:
+            plt.savefig(filename)
 class gswlc(gswlc_base):
     def __init__(self,catalog):
         self.cat = fits.getdata(catalog) 
@@ -222,7 +300,7 @@ class gswlc(gswlc_base):
         gsw_position_flag = (self.cat['RA'] > ramin) & (self.cat['RA'] < ramax) & (self.cat['DEC'] > decmin) & (self.cat['DEC'] < decmax)
         gsw_density_flag = np.log10(self.densNN*u.Mpc) < .2
         self.field1 = gsw_position_flag & gsw_density_flag
-    def plot_field1(self):
+    def plot_field1(self,figname1=None,figname2=None):
         plt.figure(figsize=(14,8))
         plt.scatter(self.cat['RA'],self.cat['DEC'],c=np.log10(self.densNN*u.Mpc),vmin=-.5,vmax=1.,s=10)
         plt.colorbar(label='$\log_{10}(N/d_N)$')
@@ -234,21 +312,31 @@ class gswlc(gswlc_base):
         plt.axvline(x=135)
         plt.axvline(x=225)
         plt.title('Entire GSWLC (LCS z range)')
+        plt.axis([130, 230, 0, 65])
+        if figname1 is not None:
+            plt.savefig(figname1)
+        if figname2 is not None:
+            plt.savefig(figname2)
+    def plot_dens_hist(self,figname1=None,figname2=None):
         plt.figure()
         t = plt.hist(np.log10(self.densNN*u.Mpc), bins=20)
         plt.xlabel('$ \log_{10} (N/d_N)$')
+
         print('median local density =  %.3f'%(np.median(np.log10(self.densNN*u.Mpc))))
         plt.axvline(x = (np.median(np.log10(self.densNN*u.Mpc))),c='k')
         plt.title('Distribution of Local Density')
-    
+        if figname1 is not None:
+            plt.savefig(figname1)
+        if figname2 is not None:
+            plt.savefig(figname2)
+
 class lcsgsw(gswlc_base):
-    def __init__(self,catalog):
+    def __init__(self,catalog,sigma_split=600):
         # read in catalog of LCS matched to GSWLC
         self.cat = fits.getdata(catalog)
         self.base_init()
-        self.calc_membflag()
-    def calc_membflag(self):
-        self.membflag = 
+        self.group = self.cat['CLUSTER_SIGMA'] < sigma_split
+        self.cluster = self.cat['CLUSTER_SIGMA'] > sigma_split        
     def get_mstar_limit(self):
         print(zmax)
         # assume hubble flow
@@ -270,32 +358,95 @@ class lcsgsw(gswlc_base):
         plt.xlabel('Mr')
         plt.ylabel('logMstar GSWLC')
         plt.grid(True)        
+    def plot_dvdr(self, figname1=None,figname2=None,plotsingle=True):
+        # log10(chabrier) = log10(Salpeter) - .25 (SFR estimate)
+        # log10(chabrier) = log10(diet Salpeter) - 0.1 (Stellar mass estimates)
+        xmin,xmax,ymin,ymax = 0,3.5,0,3.5
+        if plotsingle:
+            plt.figure(figsize=(8,6))
+            ax=plt.gca()
+            plt.subplots_adjust(left=.1,bottom=.15,top=.9,right=.9)
+            plt.ylabel('$ \Delta v/\sigma $',fontsize=26)
+            plt.xlabel('$ \Delta R/R_{200}  $',fontsize=26)
+            plt.legend(loc='upper left',numpoints=1)
+
+        if USE_DISK_ONLY:
+            clabel=['$R_{24}/R_d$','$R_{iso}(24)/R_{iso}(r)$']
+        else:
+            clabel=['$R_e(24)/R_e(r)$','$R_{iso}(24)/R_{iso}(r)$']
+        
+        x=(self.cat['DR_R200'])
+        y=abs(self.cat['DELTA_V'])
+        plt.hexbin(x,y,extent=(xmin,xmax,ymin,ymax),cmap='gray_r',gridsize=50,vmin=0,vmax=10)
+        xl=np.arange(0,2,.1)
+        plt.plot(xl,-4./3.*xl+2,'k-',lw=3,color=colorblind1)
+        props = dict(boxstyle='square', facecolor='0.8', alpha=0.8)
+        plt.text(.1,.1,'CORE',transform=plt.gca().transAxes,fontsize=18,color=colorblind3,bbox=props)
+        plt.text(.6,.6,'EXTERNAL',transform=plt.gca().transAxes,fontsize=18,color=colorblind3,bbox=props)        
+        #plt.plot(xl,-3./1.2*xl+3,'k-',lw=3)
+        plt.axis([xmin,xmax,ymin,ymax])
+        if figname1 is not None:
+            plt.savefig(figname1)
+        if figname2 is not None:
+            plt.savefig(figname2)
+
 
 class comp_lcs_gsw():
     def __init__(self,lcs,gsw,minmstar = 9.8, minssfr = -11.5):
         self.lcs = lcs
         self.gsw = gsw
-        self.minmstar = minmstar
-        self.minssfr = minssfr
+        self.masscut = minmstar
+        self.ssfrcut = minssfr
     
     def plot_ssfr_mstar(self):
 
         pass
 
-    def plot_sfr_mstar(self):
-        flag1 = lcsgsw['membflag'] &  (lcsgsw['logMstar']> masscut)  & (ssfr > ssfrcut)
-        flag2 = (gsw['logMstar'] > masscut) & (gswssfr > ssfrcut)  & field_gsw
+    def plot_sfr_mstar(self,lcsflag=None,outfile1=None,outfile2=None):
+        if lcsflag is None:
+            lcsflag = self.lcs.cat['membflag']
+        
+        flag1 = lcsflag &  (self.lcs.cat['logMstar']> self.masscut)  & (self.lcs.ssfr > self.ssfrcut)
+        flag2 = (self.gsw.cat['logMstar'] > self.masscut) & (self.gsw.ssfr > self.ssfrcut)  & self.gsw.field1
         print('number in core sample = ',sum(flag1))
         print('number in external sample = ',sum(flag2))
-        x1 = self.lcs['logMstar'][flag1]
-        y1 = self.lcs['logSFR'][flag1]
-        x2 = self.gsw['logMstar'][flag1]
-        y2 = self.gsw['logSFR'][flag1]
+        x1 = self.lcs.cat['logMstar'][flag1]
+        y1 = self.lcs.cat['logSFR'][flag1]
+        x2 = self.gsw.cat['logMstar'][flag2]
+        y2 = self.gsw.cat['logSFR'][flag2]
         
-        colormass(self.lcs['logMstar'][flag1],self.lcs[[flag1],gsw['logMstar'][flag2],gsw['logSFR'][flag2],'LCS core','GSWLC','sfr-mstar-gswlc-field.pdf',ymin=-2,ymax=1.6,xmin=9.75,xmax=11.5,nhistbin=10,ylabel='$\log_{10}(SFR)$',contourflag=False,alphagray=.8)
-        plt.savefig(homedir+'/research/LCS/plots/lcscore-gsw-sfms.pdf')
-plt.savefig(homedir+'/research/LCS/plots/lcscore-gsw-sfms.png')        
-        pass
+        colormass(x1,y1,x2,y2,'LCS core','GSWLC','sfr-mstar-gswlc-field.pdf',ymin=-2,ymax=1.6,xmin=9.75,xmax=11.5,nhistbin=10,ylabel='$\log_{10}(SFR)$',contourflag=False,alphagray=.8)
+        if outfile1 is None:
+            plt.savefig(homedir+'/research/LCS/plots/lcscore-gsw-sfms.pdf')
+        else:
+            plt.savefig(outfile1)
+        if outfile2 is None:
+            plt.savefig(homedir+'/research/LCS/plots/lcscore-gsw-sfms.png')
+        else:
+            plt.savefig(outfile2)
+    def plot_ssfr_mstar(self,lcsflag=None,outfile1=None,outfile2=None):
+        if lcsflag is None:
+            lcsflag = self.lcs.cat['membflag']
+        
+        flag1 = lcsflag &  (self.lcs.cat['logMstar']> self.masscut)  & (self.lcs.ssfr > self.ssfrcut)
+        flag2 = (self.gsw.cat['logMstar'] > self.masscut) & (self.gsw.ssfr > self.ssfrcut)  & self.gsw.field1
+        print('number in core sample = ',sum(flag1))
+        print('number in external sample = ',sum(flag2))
+        x1 = self.lcs.cat['logMstar'][flag1]
+        y1 = self.lcs.ssfr[flag1]
+        x2 = self.gsw.cat['logMstar'][flag2]
+        y2 = self.gsw.ssfr[flag2]
+        
+        colormass(x1,y1,x2,y2,'LCS core','GSWLC','sfr-mstar-gswlc-field.pdf',ymin=-11.6,ymax=-8.75,xmin=9.75,xmax=11.5,nhistbin=10,ylabel='$\log_{10}(sSFR)$',contourflag=False,alphagray=.8)
+        if outfile1 is None:
+            plt.savefig(homedir+'/research/LCS/plots/lcscore-gsw-ssfrmstar.pdf')
+        else:
+            plt.savefig(outfile1)
+        if outfile2 is None:
+            plt.savefig(homedir+'/research/LCS/plots/lcscore-gsw-ssfrmstar.png')
+        else:
+            plt.savefig(outfile2)
+
 if __name__ == '__main__':
     trimgswlc = False
     if trimgswlc:
